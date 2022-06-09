a2f30612fd31cc2c65b6f571b6bd1a1dd19c27c7
aliiev <aliiev.akhtem@gmail.com>
2022-06-10 02:16:18 +0300

    Created template

--- src/App.js
+++ src/App.js
@@ -2,10 +2,19 @@ import './App.sass'
 
 const App = () => {
   return (
-    <div className="container mt-3">
-      <h1>Hello world</h1>
-      <p className="text-secondary">Lorem ipsum dolor sit amet</p>
-      <button className="btn btn-primary">Generate</button>
+    <div className="container">
+      <div className="row min-vh-100 justify-content-center align-items-center">
+        <div className="col-12 col-md-4">
+          <div className="card border-0 rounded-4">
+            <div className="card-body">
+              <div className="qr-code bg-primary rounded-4 mb-3"></div>
+              <h2 className="text-center mb-3">QR Code Generator</h2>
+              <textarea className="form-control mb-3 bg-white"></textarea>
+              <p className="text-secondary text-center">Enter your website or text. Your QR Code will be generated automatically</p>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   )
 }
