944f01d2ef754b7778fe478b700db0b3304e9f55
aliiev <aliiev.akhtem@gmail.com>
2022-06-10 02:33:20 +0300

    Added QRCode Component

--- src/App.js
+++ src/App.js
@@ -1,13 +1,19 @@
+import QRCode from 'react-qr-code'
 import './App.sass'
 
 const App = () => {
   return (
     <div className="container">
       <div className="row min-vh-100 justify-content-center align-items-center">
-        <div className="col-12 col-md-4">
+        <div className="col-12 col-md-6 col-lg-4">
           <div className="card border-0 rounded-4">
             <div className="card-body">
-              <div className="qr-code bg-primary rounded-4 mb-3"></div>
+              <div className="qr-code-wrapper bg-primary rounded-4 mb-3">
+                <QRCode
+                  className="qr-code"
+                  value="https://github.com/aliiev/react-qr-code-generator"
+                  fgColor="transparent" />
+              </div>
               <h2 className="text-center mb-3">QR Code Generator</h2>
               <textarea className="form-control mb-3 bg-white"></textarea>
               <p className="text-secondary text-center">Enter your website or text. Your QR Code will be generated automatically</p>
