b60af026e821f7a93b9aa0d2a5b0687e3224fade
aliiev <aliiev.akhtem@gmail.com>
2022-06-10 02:38:29 +0300

    Added functionality

--- src/App.js
+++ src/App.js
@@ -1,7 +1,10 @@
+import { useState } from 'react'
 import QRCode from 'react-qr-code'
 import './App.sass'
 
 const App = () => {
+  const [state, setState] = useState('https://github.com/aliiev/react-qr-code-generator')
+
   return (
     <div className="container">
       <div className="row min-vh-100 justify-content-center align-items-center">
@@ -11,11 +14,15 @@ const App = () => {
               <div className="qr-code-wrapper bg-primary rounded-4 mb-3">
                 <QRCode
                   className="qr-code"
-                  value="https://github.com/aliiev/react-qr-code-generator"
+                  value={ state }
                   fgColor="transparent" />
               </div>
               <h2 className="text-center mb-3">QR Code Generator</h2>
-              <textarea className="form-control mb-3 bg-white"></textarea>
+              <textarea
+                className="form-control bg-white mb-3"
+                value={ state }
+                onChange={ e => setState(e.target.value) }>
+              </textarea>
               <p className="text-secondary text-center">Enter your website or text. Your QR Code will be generated automatically</p>
             </div>
           </div>
